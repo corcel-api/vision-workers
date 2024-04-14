31b0eec0db7fbeef1a85ba0b008ba75a9c4d2044
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-14 21:23:53 +0200

    debug

--- ecosystem_orchestrator.config.js
+++ ecosystem_orchestrator.config.js
@@ -5,13 +5,15 @@ module.exports = {
         script: "./run_autoupdater.py",
         args: "--restart_script /orchestrator_autoupdater_action.sh",
         interpreter: "python",
-        watch: false
+        watch: false,
+        autorestart: false
       },
       {
         name: "entrypoint",
         script: "./entrypoint.sh",
         cwd: "/validator_orchestrator",
-        watch: false
+        watch: false,
+        autorestart: false
       }
     ]
   };
