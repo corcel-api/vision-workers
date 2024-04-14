e008a9b5497ccf7125f8fff58b51c6a98008490f
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-14 17:38:34 +0200

    adding pm2 management, output to stdout/stderr

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: "autoupdater",
+        script: "./run_autoupdater.py",
+        args: "--restart_script /app/orchestrator_autoupdater_action.sh",
+        interpreter: "python",
+        watch: false
+      },
+      {
+        name: "entrypoint",
+        script: "./entrypoint.sh",
+        cwd: "/app/validator_orchestrator",
+        watch: false
+      }
+    ]
+  };
+  
