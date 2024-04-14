b5c3b8f31a8952cf476a74d25283a014be47d10d
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-14 19:04:47 +0200

    remove --reload from orchestrator entrypoint uvicorn cmd

--- validator_orchestrator/entrypoint.sh
+++ validator_orchestrator/entrypoint.sh
@@ -3,5 +3,5 @@
 port=${PORT:-6920}
 
 source activate venv
-uvicorn --lifespan on --reload --port $port --host 0.0.0.0 app.asgi:app 
+uvicorn --lifespan on --port $port --host 0.0.0.0 app.asgi:app 
 
