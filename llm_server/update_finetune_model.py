18201c03aacfeda354abfa513d2af83b63c825a9
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-15 20:20:36 +0200

    ping every 1 minute

--- llm_server/update_finetune_model.py
+++ llm_server/update_finetune_model.py
@@ -17,10 +17,9 @@ class DatabaseClient:
             return self.client['llm_server_miners']
         except ConnectionFailure as e:
             print(f"[{datetime.now()}] MongoDB connection failed: {e}")
-            exit(1)  # Exit if the connection is not successful
+            exit(1) 
 
     def get_latest_model_payload(self):
-        # Fetch the payload directly from the MongoDB document
         doc = self.db.best_finetune_model.find_one({'id': 'best_finetune_model'})
         if doc:
             return {key: value for key, value in doc.items() if key != '_id'}
@@ -33,7 +32,6 @@ def load_new_model(payload):
     return response.status_code, response.text
 
 def main():
-    # Replace the URI below with your actual MongoDB Atlas URI
     uri = "mongodb+srv://miner_llm:ByjJThQcxGL7QOdT@cluster0.qnwgewf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     db_client = DatabaseClient(uri)
     last_loaded_model_payload = db_client.get_latest_model_payload()
@@ -52,7 +50,7 @@ def main():
         except Exception as e:
             print(f"[{datetime.now()}] Error: {e}")
 
-        time.sleep(10)  # Sleep for one minute
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
