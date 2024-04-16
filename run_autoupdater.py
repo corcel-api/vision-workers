ec2e3e36d9732627b97934516a054034229c9345
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-16 13:12:41 +0200

    only restard when server special token is mentionned

--- run_autoupdater.py
+++ run_autoupdater.py
@@ -62,21 +62,24 @@ def print_changes_since_last_tag(local_tag, remote_tag):
     else:
         print("No changes were found, or the tags are identical.\n")
 
-def run_autoupdate(restart_script: str, special_token: str, ports_to_kill: list, branch: str, auto_updates_sleep: int):
+def run_autoupdate(restart_script: str, env_autoup_token: str, server_special_token: str, ports_to_kill: list, branch: str, auto_updates_sleep: int):
     while True:
         fetch_latest_tags(branch)
         local_tag = get_latest_tag()
         remote_tag = get_latest_remote_tag(branch)
 
-        if should_update(local_tag, remote_tag) and contains_special_token(remote_tag, special_token):
+        if should_update(local_tag, remote_tag) and contains_special_token(remote_tag, env_autoup_token):
             print(f"Local repository is not up-to-date. Updating...")
             update_repository(remote_tag)
             print_changes_since_last_tag(local_tag, remote_tag)
-            print(f"Remote tag contains required token. Running the autoupdate steps...")
-            stop_server_on_port(ports_to_kill)
-            subprocess.run(f"chmod +x {restart_script}", shell=True)
-            subprocess.Popen(f"/bin/sh {restart_script}", shell=True)
-            print("Finished running the autoupdate steps! Server is ready.")
+            if contains_special_token(remote_tag, server_special_token):
+                print(f"Remote tag contains required token. Running the autoupdate steps...")
+                stop_server_on_port(ports_to_kill)
+                subprocess.run(f"chmod +x {restart_script}", shell=True)
+                subprocess.Popen(f"/bin/sh {restart_script}", shell=True)
+                print("Finished running the autoupdate steps! Server is ready.")
+            else:
+                print("Local repository is up-to-date, no restard needed since server token was not mentioned")
         else:
             print("Local repository is up-to-date or the tag does not contain the required token.")
         time.sleep(auto_updates_sleep)
@@ -89,17 +92,19 @@ if __name__ == "__main__":
     auto_updates_sleep = int(os.environ.get('AUTOUP_SLEEP', '60'))
     current_env = os.environ.get('CURRENT_ENV', 'dev').lower()
     branch = os.environ.get('GIT_BRANCH', 'main' if current_env == 'prod' else 'dev')
-    special_token = "_prod" if current_env == "prod" else "_dev"
+    server_special_token = os.environ.get('SERVER_RELOAD_GIT_TOKEN', 'none')
+    env_autoup_token = "_prod" if current_env == "prod" else "_dev"
 
     orchestrator_port = int(os.environ.get('CURRENT_SERVER_PORT', 6920))
     service_port = int(os.environ.get('SERVICE_SERVER_PORT', 6919))
     comfyui_port = int(os.environ.get('COMFYUI_SERVER_PORT', 8188))
 
     print(f"\nEnvironment: {current_env.upper()}")
-    print(f"Listening for Git tag updates on branch '{branch}' with tags containing the token: '{special_token}'\n")
+    print(f"Listening for Git tag updates on branch '{branch}' with tags containing the tokens: '{env_autoup_token}, {server_special_token}'\n")
 
     run_autoupdate(restart_script=args.restart_script, 
-                   special_token=special_token, 
+                   env_autoup_token=env_autoup_token, 
+                   server_special_token=server_special_token,
                    ports_to_kill=[orchestrator_port, service_port, comfyui_port],
                    branch=branch,
                    auto_updates_sleep=auto_updates_sleep)
