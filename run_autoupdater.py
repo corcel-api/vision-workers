9e53b25c29817c186490891f17e8a5418bde2cd5
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-19 22:39:13 +0200

    adding prefix for private functions

--- run_autoupdater.py
+++ run_autoupdater.py
@@ -7,27 +7,27 @@ from typing import List
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_latest_tag() -> str:
+def _get_latest_tag() -> str:
     return subprocess.getoutput("git describe --tags --abbrev=0")
 
-def get_latest_remote_tag(branch: str) -> str:
+def _get_latest_remote_tag(branch: str) -> str:
     subprocess.run(["git", "fetch", "--tags", "origin", branch], check=True)
     latest_commit = subprocess.getoutput(f"git rev-list --tags --max-count=1 --branches=origin/{branch}")
     return subprocess.getoutput(f"git describe --tags --abbrev=0 {latest_commit}")
 
-def fetch_latest_tags(branch: str) -> None:
+def _fetch_latest_tags(branch: str) -> None:
     subprocess.run(["git", "fetch", "--tags", "origin", branch], check=True)
 
-def should_update(local_tag: str, remote_tag: str) -> bool:
+def _should_update(local_tag: str, remote_tag: str) -> bool:
     logging.info(f"Comparing local tag: {local_tag} to remote tag: {remote_tag}")
     return local_tag != remote_tag
 
-def update_repository(target_tag: str) -> None:
+def _update_repository(target_tag: str) -> None:
     logging.info(f"Updating to tag: {target_tag}")
     subprocess.run(["git", "reset", "--hard", target_tag], check=True)
     logging.info("Repository updated to the latest tag.")
 
-def stop_server_on_port(ports_to_kill: List[int]) -> None:
+def _stop_server_on_port(ports_to_kill: List[int]) -> None:
     for port in ports_to_kill:
         logging.info(f"Stopping the server on port {port}...")
         try:
@@ -45,10 +45,10 @@ def stop_server_on_port(ports_to_kill: List[int]) -> None:
                 time.sleep(1)
     logging.info('All servers stopped successfully!')
 
-def contains_special_token(tag: str, token: str) -> bool:
+def _contains_special_token(tag: str, token: str) -> bool:
     return token in tag
 
-def print_changes_since_last_tag(local_tag: str, remote_tag: str) -> None:
+def _print_changes_since_last_tag(local_tag: str, remote_tag: str) -> None:
     logging.info("Changes since last update:")
     changes = subprocess.getoutput(f"git log {local_tag}..{remote_tag} --pretty=format:'%h - %s (%cd) [%an]' --date=format:'%Y-%m-%d %H:%M:%S'")
     if changes:
@@ -58,17 +58,17 @@ def print_changes_since_last_tag(local_tag: str, remote_tag: str) -> None:
 
 def run_autoupdate(restart_script: str, env_autoup_token: str, server_special_token: str, ports_to_kill: List[int], branch: str, auto_updates_sleep: int) -> None:
     while True:
-        fetch_latest_tags(branch)
-        local_tag = get_latest_tag()
-        remote_tag = get_latest_remote_tag(branch)
+        _fetch_latest_tags(branch)
+        local_tag = _get_latest_tag()
+        remote_tag = _get_latest_remote_tag(branch)
 
-        if should_update(local_tag, remote_tag) and contains_special_token(remote_tag, env_autoup_token):
+        if _should_update(local_tag, remote_tag) and _contains_special_token(remote_tag, env_autoup_token):
             logging.info("Local repository is not up-to-date. Updating...")
-            update_repository(remote_tag)
-            print_changes_since_last_tag(local_tag, remote_tag)
-            if contains_special_token(remote_tag, server_special_token):
+            _update_repository(remote_tag)
+            _print_changes_since_last_tag(local_tag, remote_tag)
+            if _contains_special_token(remote_tag, server_special_token):
                 logging.info("Remote tag contains required token. Running the autoupdate steps...")
-                stop_server_on_port(ports_to_kill)
+                _stop_server_on_port(ports_to_kill)
                 subprocess.run(f"chmod +x {restart_script}", shell=True)
                 subprocess.Popen(f"/bin/sh {restart_script}", shell=True)
                 logging.info("Finished running the autoupdate steps! Server is ready.")
