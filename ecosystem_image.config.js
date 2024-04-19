58b29aec6cbca5c4dfd2bee8d6b03990ad1eeece
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-19 20:37:37 +0200

    removing auto updates for miners

--- ecosystem_image.config.js
+++ ecosystem_image.config.js
@@ -1,14 +1,6 @@
 module.exports = {
     apps: [
       {
-        name: "autoupdater",
-        script: "./run_autoupdater.py",
-        args: "--restart_script /image_autoupdater_action.sh",
-        interpreter: "python",
-        watch: false,
-        autorestart: false
-      },
-      {
         name: "entrypoint",
         script: "./entrypoint.sh",
         cwd: "/image_server",
--- ecosystem_llm.config.js
+++ ecosystem_llm.config.js
@@ -9,14 +9,6 @@ module.exports = {
         autorestart: true
       },
       {
-        name: "autoupdater",
-        script: "./run_autoupdater.py",
-        args: "--restart_script /llm_autoupdater_action.sh",
-        interpreter: "python",
-        watch: false,
-        autorestart: false
-      },
-      {
         name: "entrypoint",
         script: "./entrypoint.sh",
         cwd: "/llm_server",
