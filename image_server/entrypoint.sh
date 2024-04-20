5ffcecf9d3e33870b0e09a99b250aad5b3212477
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-20 15:26:31 +0200

    fix Paths

--- image_server/entrypoint.sh
+++ image_server/entrypoint.sh
@@ -20,9 +20,9 @@ port=${PORT:-6919}
 ls -la 
 if [ -n "$vram_mode" ]
 then
-    python /ComfyUI/main.py $vram_mode --cuda-device $device --disable-xformers &
+    python ./ComfyUI/main.py $vram_mode --cuda-device $device --disable-xformers &
 else
-    python /ComfyUI/main.py --disable-xformers  --cuda-device $device &
+    python ./ComfyUI/main.py --disable-xformers  --cuda-device $device &
 fi
 
 
