03599ddcc436b5bc431383ad5e7f69a5b7db0248
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-22 12:26:09 +0200

    remove finetune model autoupdate (endpoint pinning+load_model call)

--- ecosystem_llm.config.js
+++ ecosystem_llm.config.js
@@ -1,14 +1,6 @@
 module.exports = {
     apps: [
       {
-        name: "fineTuneModelUpdate",
-        script: "./update_finetune_model.py",
-        interpreter: "python",
-        cwd: "/app/llm_server",
-        watch: false,
-        autorestart: true
-      },
-      {
         name: "entrypoint",
         script: "./entrypoint.sh",
         cwd: "/app/llm_server",
