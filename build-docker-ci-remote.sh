4c707fc3f977f1a7e7f4cf29428f2400d5a44ff7
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-04-24 19:00:04 +0200

    adding llm server workflow + cleaning

--- build-docker-ci-remote.sh
+++ build-docker-ci-remote.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker build -f Dockerfile.orchestrator -t corcelio/cicd:$BUILD_ID .
-docker push corcelio/cicd:$BUILD_ID
+docker build -f Dockerfile.orchestrator -t corcelio/cicd:orchestrator-$BUILD_ID .
+docker push corcelio/cicd:orchestrator-$BUILD_ID
+
+docker build -f Dockerfile.llm_server -t corcelio/cicd:llm-server-$BUILD_ID .
+docker push corcelio/cicd:llm-server-$BUILD_ID
+
+docker build -f Dockerfile.image_server -t corcelio/cicd:image-server-$BUILD_ID .
+docker push corcelio/cicd:image-server-$BUILD_ID
