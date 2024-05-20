87d36488e0eca9415198bb61ca708abb5cac1bbf
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-05-20 14:20:49 +0200

    upgrade vllm

--- llm_server/app/inference/state.py
+++ llm_server/app/inference/state.py
@@ -1,7 +1,7 @@
 import gc
 
 import torch
-from vllm.model_executor.parallel_utils.parallel_state import destroy_model_parallel
+from vllm.distributed.parallel_state import destroy_model_parallel
 from app.logging import logging
 from app import models
 from app.inference import engines, completions, toxic
